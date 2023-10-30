7b723368bfb7aa11175b88184c5d04d3a06f55f1
alesacconi <136035610+alesacconi@users.noreply.github.com>
2023-10-30 16:46:10 -0300

    Add files via upload

---
+++ NOSE/identidad/nose.js
@@ -0,0 +1,7 @@
+function añadir(){
+
+    nombre= document.getElementById("nombre").value
+    apellid = document.getElementById("apellido").value
+    dni = document.getElementById("dni").value
+    alert ("Bienvenido SR" + nombre + apellid + "su apellidoes" + dni)
+}
